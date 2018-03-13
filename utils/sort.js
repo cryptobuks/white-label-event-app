3848d434f40f9451a9af235794f1d99e7ca8d129
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2018-03-13 16:08:02 +0100

    Update sorting function to sort in two orders.

--- utils/sort.js
+++ utils/sort.js
@@ -1,1 +1,7 @@
-export const SortByDate = trackSessions => trackSessions.sort((a, b) => new Date(a.date) - new Date(b.date));
+export const sortByDate = (trackSessions, order = 'ASC') => {
+  if (order.toUpperCase() === 'DESC') {
+    return [...trackSessions.sort((a, b) => new Date(b.date) - new Date(a.date))];
+  }
+  
+  return [...trackSessions.sort((a, b) => new Date(a.date) - new Date(b.date))];
+};
