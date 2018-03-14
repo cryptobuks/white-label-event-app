508ed070c165677af2839111b0e4b6d413aa9fde
Mike Verfaillie <mike.verfaillie@inthepocket.mobi>
2018-03-14 09:19:11 +0100

    add disabled styling for personalSchedule button

--- components/personalScheduleButton.js
+++ components/personalScheduleButton.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native';
 import { COLORS, FONT_SIZES, METRICS, IS_IPHONE_X, FONT_WEIGHTS } from '../config';
 
+// Dummy boolean, will be calculated based on content of subscribedTracks in state
+const DISABLED = false;
+
 const SHADOW = {
   offset: { x: -5 },
   opacity: 0.5,
@@ -32,11 +35,19 @@ const styles = StyleSheet.create({
     paddingBottom: METRICS.gridSize * 3,
     height: METRICS.gridSize * 10,
   },
+  disabled: {
+    backgroundColor: COLORS.grey,
+  },
 });
 
 const PersonalScheduleButton = ({ onPress }) => (
-  <TouchableWithoutFeedback onPress={onPress}>
-    <View style={[styles.buttonContainer, IS_IPHONE_X ? styles.iPhoneXPaddingBottom : null]}>
+  <TouchableWithoutFeedback onPress={!DISABLED ? onPress : null}>
+    <View style={[
+      styles.buttonContainer,
+      IS_IPHONE_X ? styles.iPhoneXPaddingBottom : null,
+      DISABLED ? styles.disabled : null,
+    ]}
+    >
       <Text style={styles.buttonText}>{'My Schedule'}</Text>
     </View>
   </TouchableWithoutFeedback>
