cab4d96193a44ed7ef1eec897cfc048c4c8f3661
Mike Verfaillie <mike.verfaillie@inthepocket.mobi>
2018-03-20 11:54:38 +0100

    update filenames

---
+++ components/Loading.js
@@ -0,0 +1,39 @@
+// @flow
+import React from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import type { TStyleSheet } from '../types/stylesheet';
+import { COLORS } from '../config';
+
+type TProps = {
+  animating?: boolean,
+  hidesWhenStopped?: boolean,
+  size?: number | 'small' | 'large',
+} & TStyleSheet;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+});
+
+const Loading = ({ animating, color, hidesWhenStopped, size }: TProps) => (
+  <View style={styles.container}>
+    <ActivityIndicator
+      animating={animating}
+      color={color}
+      hidesWhenStopped={hidesWhenStopped}
+      size={size}
+    />
+  </View>
+);
+
+Loading.defaultProps = {
+  animating: true,
+  color: COLORS.black,
+  hidesWhenStopped: true,
+  size: 'large',
+};
+
+export default Loading;
