cbaecf43f0eb0baf8d85f304ef331f96c508bc67
Mike Verfaillie <mike.verfaillie@inthepocket.mobi>
2018-03-20 12:50:29 +0100

    remove braces around strings

--- screens/login/LoginScreen.js
+++ screens/login/LoginScreen.js
@@ -44,12 +44,12 @@ const LoginScreen = ({ onFacebookLogin, onGoogleLogin }: TProps) => (
     <View style={styles.buttons}>
       <TouchableHighlight onPress={onFacebookLogin}>
         <View style={styles.button}>
-          <Text style={styles.buttonText}>{'Login with Facebook'}</Text>
+          <Text style={styles.buttonText}>Login with Facebook</Text>
         </View>
       </TouchableHighlight>
       <TouchableHighlight onPress={onGoogleLogin}>
         <View style={styles.button}>
-          <Text style={styles.buttonText}>{'Login with Google'}</Text>
+          <Text style={styles.buttonText}>Login with Google</Text>
         </View>
       </TouchableHighlight>
     </View>
